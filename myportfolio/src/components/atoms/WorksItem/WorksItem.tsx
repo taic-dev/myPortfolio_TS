c0aaef3c2e930602a52e348bbcb93661d054a16e
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-05 20:35:11 +0900

    :add: #9 descの表示を最大50文字に制限

--- myportfolio/src/components/atoms/WorksItem/WorksItem.tsx
+++ myportfolio/src/components/atoms/WorksItem/WorksItem.tsx
@@ -23,7 +23,7 @@ export const WorksItem = ({
         <img src={imgUrl} alt="実績画像" className='w-[100%] shadow-md mb-[20px] rounded-xl' />
         <div className='leading-none'>
           <h4 className='text-lg mb-[10px] font-bold noto-sans-japanese'>{name.length > 25 ? `${name.slice(0,25)}...` : name }</h4>
-          <p className='text-sm mb-[10px] noto-sans-japanese'>{desc}</p>
+          <p className='text-sm mb-[10px] noto-sans-japanese'>{desc.length >= 40 ? `${desc.slice(0,40)}...` : desc }</p>
           <span className='text-xs noto-sans-japanese'>{year} / {client}</span>
         </div>
       </a>
