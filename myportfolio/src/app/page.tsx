2fdcb8b4c3689496fd65c7c48ff740b8d682eea4
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-05 13:31:06 +0900

    :add: #7 propsとしてコンポーネントに情報を渡した

--- myportfolio/src/app/page.tsx
+++ myportfolio/src/app/page.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { SkillLabelList } from '@/components/molecules/SkillLabelList/SkillLabelList'
 import { getSkills } from "@/libs/microcms";
+import type { MicroCMSImage } from "microcms-js-sdk";
+
+interface SkillsType {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  level: number;
+  iconUrl: MicroCMSImage;
+}
 
 export default async function Home() {
-  const { contents } = await getSkills();
-
-  console.log(contents)
+  const skillLists = await getSkills();
   return (
      <main>
-      <SkillLabelList />
+      <SkillLabelList skillLists={skillLists} />
      </main>
   )
 }
