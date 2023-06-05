eafb00209d6cc7125fe26dad2a3fe94c08e3d2dd
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-05 20:25:19 +0900

    :add: #9 worksの情報の取得&表示

--- myportfolio/src/app/page.tsx
+++ myportfolio/src/app/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SkillLabelList } from '@/components/molecules/SkillLabelList/SkillLabelList'
+import { WorksList } from '@/components/molecules/WorksList/WorksList';
 import { getSkills, getWorks } from "@/libs/microcms";
 
 export default async function Home() {
   const skillLists = await getSkills();
+  const worksLists = await getWorks();
   return (
      <main>
       <SkillLabelList skillLists={skillLists} />
-      
+      <WorksList worksLists={worksLists} />
      </main>
   )
 }
