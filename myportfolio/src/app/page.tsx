87ddbcdf71c0a27382f820fd86e3cd6639c1ded9
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-06 14:58:08 +0900

    :add: #13 Skill Development language作成完了

--- myportfolio/src/app/page.tsx
+++ myportfolio/src/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Sidebar } from '@/components/templates/Sidebar';
 import { FvSection } from '@/components/organisms/FvSection/FvSection';
 import { AboutSection } from '@/components/organisms/AboutSection/AboutSection';
+import { SkillSection } from '@/components/organisms/SkillSection/SkillSection';
 import { SkillLabelList } from '@/components/molecules/SkillLabelList/SkillLabelList'
 import { WorksList } from '@/components/molecules/WorksList/WorksList';
 import { getSkills, getWorks } from "@/libs/microcms";
+import { Headline3 } from '@/components/atoms/Headline3/Headline3';
 
 export default async function Home() {
   const skillLists = await getSkills();
@@ -16,7 +18,14 @@ export default async function Home() {
         <main>
           <FvSection />
           <AboutSection />
-          <SkillLabelList skillLists={skillLists} />
+          <SkillSection>
+            <Headline3 name="Front end" />
+            <SkillLabelList skillLists={skillLists} />
+            <Headline3 name="Back end" />
+            <SkillLabelList skillLists={skillLists} />
+            <Headline3 name="Other" />
+            <SkillLabelList skillLists={skillLists} />
+          </SkillSection>
           <WorksList worksLists={worksLists} />
         </main>
       </div>
--- myportfolio/src/components/organisms/AboutSection/AboutSection.tsx
+++ myportfolio/src/components/organisms/AboutSection/AboutSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Headline1 } from '@/components/atoms/Headline1/Headline1'
 import { Headline2 } from '@/components/atoms/Headline2/Headline2'
-import { Headline3 } from '@/components/atoms/Headline3/Headline3'
+import { Headline4 } from '@/components/atoms/Headline4/Headline4'
 
 export function AboutSection() {
   return (
@@ -11,23 +11,23 @@ export function AboutSection() {
       <div className="ml-[50px]">
         <Headline2 name="Profile" />
         <div className="flex items-center">
-          <Headline3 name="Name" />
+          <Headline4 name="Name" />
           <p>Taishi Ohmoto（大本 泰史）</p>
         </div>
         <div className="flex items-center">
-          <Headline3 name="Date of birth" />
+          <Headline4 name="Date of birth" />
           <p>1998.5.30</p>
         </div>
         <div className="flex items-center">
-          <Headline3 name="Birthplace" />
+          <Headline4 name="Birthplace" />
           <p>Japan / Hiroshima</p>
         </div>
         <div className="flex items-center">
-          <Headline3 name="Hobby" />
+          <Headline4 name="Hobby" />
           <p>Fashion,Sauna</p>
         </div>
         <div className="flex items-center">
-          <Headline3 name="SNS" />
+          <Headline4 name="SNS" />
           <div className='flex items-center gap-5'>
             <a href="https://github.com/taic-dev">
               <Image src="/github.svg" width={40} height={40} alt="Githubロゴ" className='h-auto' />
@@ -37,7 +37,6 @@ export function AboutSection() {
             </a>
           </div>
         </div>
-        
       </div>
     </section>
   )
