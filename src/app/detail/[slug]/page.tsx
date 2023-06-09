8e8578279bfb095c4cd901f9b357d75971de4fa9
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-09 23:09:48 +0900

    :add: #25 詳細ページの情報を表示

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,10 +1,44 @@
 import React from "react"
+import Image from 'next/image';
+import { getWorks } from "@/libs/microcms";
+import type { MicroCMSImage } from "microcms-js-sdk";
 
-export default function worksDetail({ params }: {params : {slug: string}}) {
+interface WorksType {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  desc: string;
+  date: number;
+  client: string;
+  thumbnail: MicroCMSImage;
+  web_url?: string;
+  github_url?: string;
+  frontend?: [];
+  backend?: [];
+  other?: [];
+}
+
+export default async function worksDetail({ params }: {params : {slug: string}}) {
+  const worksLists = await getWorks();
+  const worksList = worksLists.contents.filter((v: WorksType)=>{
+    return v.id === params.slug && v; 
+  });
+
+  const {id, name, desc, date, client, thumbnail:{ url }} = worksList[0]
+
   return (
     <div>
-      詳細ページです。
-      {params.slug}
+      詳細ページです
+      <Image src={url} alt="制作物の画像" width={1980} height={1150} className='w-[100%] shadow-md mb-[20px] rounded-xl'/>
+      id番号:{id}<br />
+      名前:{name}<br />
+      説明:{desc}<br />
+      年:{date}<br />
+      クライアント:{client} <br />
+
     </div>
   )
 }
