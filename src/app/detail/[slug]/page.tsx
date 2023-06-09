8ed7b83ec8a82af7e97da7cc4cfcdc86b735300b
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-10 00:05:12 +0900

    :add: #25 詳細ページの表示する情報を整理

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -27,18 +27,28 @@ export default async function worksDetail({ params }: {params : {slug: string}})
     return v.id === params.slug && v; 
   });
 
-  const {id, name, desc, date, client, thumbnail:{ url }} = worksList[0]
-
+  const {id, name, desc, date, client, web_url, github_url, frontend, backend, other, thumbnail:{ url }} = worksList[0]
+  const langList = [...frontend, ...backend, ...other];
+  
   return (
-    <main>
-      
+    <main className="mb-[50px]">
       <Image src={url} alt="制作物の画像" width={1980} height={1150} className='w-[100%] shadow-md mb-[20px] rounded-xl'/>
-      id番号:{id}<br />
-      名前:{name}<br />
-      説明:{desc}<br />
-      年:{date}<br />
-      クライアント:{client} <br />
-
+      <h1 className="text-3xl font-bold mt-12 mb-7 pb-7 relative before:absolute before:bottom-0 before:w-[50px] before:h-[4px] before:bg-black before:content-['']">{name}</h1>
+      <div className="mb-7">
+        <p>{date} / {client}</p>
+      </div>
+      <div className="flex gap-[10px] mb-7">
+        {web_url && <a href={web_url}>web_url</a>}
+        {github_url && <a href={github_url}>Github</a>}
+      </div>
+      <ul className="flex gap-[10px] mb-7">
+        {langList.map((v)=>(
+          <li>{v}</li>
+        ))}
+      </ul>
+      <div>
+        <p>{desc}</p>
+      </div>
     </main>
   )
 }
