817c1a9b4cfa8cccc46468722cd735cff4e92a12
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-22 17:22:54 +0900

    :add: headline1 アニメーション追加

--- src/components/atoms/Headline1/Headline1.tsx
+++ src/components/atoms/Headline1/Headline1.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+
+import React from "react";
+import { motion } from "framer-motion";
 
 interface Headline1Props {
   name: string;
@@ -7,9 +10,37 @@ interface Headline1Props {
 
 export const Headline1 = ({ name, subName }: Headline1Props) => {
   return (
-    <div className='headline1 mb-8 lg:ml-[15px]'>
-      <h2 className='text-4xl'>{name}</h2>
-      <span className='text-sm'>{subName}</span>
+    <div className="headline1 mb-8 lg:ml-[15px] py-[5px] overflow-hidden">
+      <motion.h2
+        className="overflow-hidden text-4xl"
+        initial={{ y: 100 }}
+        animate={{
+          y: [100, -5, 0],
+        }}
+        transition={{
+          delay: 1,
+          duration: 1,
+        }}
+      >
+        {name}
+      </motion.h2>
+      <motion.span
+        className="text-sm"
+        initial={{ 
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{ 
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{ 
+          delay: 1,
+          duration: 1.5 
+        }}
+      >
+        {subName}
+      </motion.span>
     </div>
-  )
-}
+  );
+};
--- src/components/atoms/SidebarIcon/SidebarIcon.tsx
+++ src/components/atoms/SidebarIcon/SidebarIcon.tsx
@@ -1,7 +1,7 @@
-"use client";
+"use client"
 
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react'
+import { motion } from 'framer-motion';
 
 interface SidebarIconPops {
   link: string;
@@ -12,22 +12,19 @@ interface SidebarIconPops {
 export const SidebarIcon = ({ link, iconUrl, position }: SidebarIconPops) => {
   return (
     <motion.div
-      className="w-[200px] h-[200px] rotate-180"
       initial={{ scale: 0 }}
       animate={{ rotate: 180, scale: 1 }}
       transition={{
         type: "spring",
         stiffness: 260,
-        damping: 20,
+        damping: 20
       }}
     >
-      <a href={link}>
-        <img
-          src={iconUrl}
-          alt="アイコン画像"
-          className={`w-[200px] h-[200px] object-cover rounded-[20%] m-${position}`}
-        />
-      </a>
+      <div className='w-[200px] h-[200px] rotate-180'>
+        <a href={link}>
+          <img src={iconUrl} alt="アイコン画像" className={`w-[200px] h-[200px] object-cover rounded-[20%] m-${position}`} />
+        </a>
+      </div>
     </motion.div>
-  );
-};
+  )
+}
