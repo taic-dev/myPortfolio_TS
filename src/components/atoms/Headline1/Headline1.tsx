87cb4c2179cd50f8989b97b4471359a6a98098d5
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-23 10:36:10 +0900

    :fix: #35 タイトル1 画面内にコンテンツが入ったらアニメーション発火

--- src/components/atoms/Headline1/Headline1.tsx
+++ src/components/atoms/Headline1/Headline1.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import { motion } from "framer-motion";
@@ -11,33 +11,23 @@ interface Headline1Props {
 export const Headline1 = ({ name, subName }: Headline1Props) => {
   return (
     <div className="headline1 mb-8 lg:ml-[15px] py-[5px] overflow-hidden">
-      <motion.h2
-        className="overflow-hidden text-4xl"
-        initial={{ y: 100 }}
-        animate={{
-          y: [100, -5, 0],
-        }}
-        transition={{
-          delay: 1,
-          duration: 1,
-        }}
-      >
-        {name}
-      </motion.h2>
+      <div className="overflow-hidden">
+        <motion.h2
+          className="overflow-hidden text-4xl"
+          initial={{ y: 40 }}
+          whileInView={{ y: [40, -3, 0] }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+        >
+          {name}
+        </motion.h2>
+      </div>
       <motion.span
         className="text-sm"
-        initial={{ 
-          opacity: 0,
-          y: 10,
-        }}
-        animate={{ 
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{ 
-          delay: 1,
-          duration: 1.5 
-        }}
+        initial={{ opacity: 0, y: 10 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.5, duration: 0.5 }}
       >
         {subName}
       </motion.span>
