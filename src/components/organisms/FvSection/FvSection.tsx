88fc8f98720bc61bbe7b3c83884fc9316644e9f9
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-11-27 01:09:14 +0900

    update: vision

--- src/components/organisms/FvSection/FvSection.tsx
+++ src/components/organisms/FvSection/FvSection.tsx
@@ -62,7 +62,7 @@ export const FvSection = () => {
         }}
       >
         <p className="leading-loose noto-sans-japanese font-bold">Web開発者。HP制作、WEBアプリ開発、業務効率化ツール制作と幅広く手がけています。 中でもフロントエンド開発が大好きで、ユーザーがストレスなく使えるサービスを目指して日々開発に取り組んでいます。<br />
-          また、<strong className="underline">「自分が死んだ後も使われるようなサービスを作ること」</strong>を人生の目標にしています。</p>
+          また、<strong className="underline">「自分が死んだ後も使われるようなモノを作ること」</strong>を人生の目標にしています。</p>
       </motion.div>
     </section>
   )
